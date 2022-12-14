39c6bc1b9be04141aaf6b09fd8dfc6a69707dccf
dbp-3sem <119473300+dbp-3sem@users.noreply.github.com>
2022-12-14 18:21:06 -0500

    Add files via upload

---
+++ Formulario CV/Vistas/Otros.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace Formulario_CV.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Otros : ContentPage
+    {
+
+        String names;
+        String edads;
+        String correos;
+        String telefonos;
+        String ocupacions;
+        String direcions;
+        String info_perfils;
+        String info_idiomas;
+        bool Validacion;
+        int llenado;
+
+        public Otros(String name, String edad,
+            String correo, String telefono, String ocupacion,
+            String direcion, String info_perfil, String idiomas)
+        {
+            InitializeComponent();
+            names = name;
+            edads = edad;
+            correos = correo;
+            telefonos = telefono;
+            ocupacions = ocupacion;
+            direcions = direcion;
+            info_perfils = info_perfil;
+            info_idiomas = idiomas;
+
+        }
+
+        string redesxy;
+        string educacionxy;
+        string experienciaxy;
+        string hobbiesxy;
+
+        private async void btncompletado_Clicked(object sender, EventArgs e)
+        {
+
+            redesxy = redesx.Text;
+            educacionxy = educacionx.Text;
+            experienciaxy = experienciax.Text;
+            hobbiesxy = hobbiesx.Text;
+
+            Validacion = Validar();
+
+            if (Validacion == true)
+            {
+                await Navigation.PushAsync(new Enviar(names, edads,
+                correos, telefonos, ocupacions,
+                direcions, info_perfils, info_idiomas, redesxy, educacionxy,
+                experienciaxy, hobbiesxy));
+            }
+
+        }
+
+
+        private async void btncompletado2_Clicked(object sender, EventArgs e)
+        {
+            var curriculum = new EmailMessage("","","");
+            curriculum.BodyFormat = EmailBodyFormat.Html;
+            await Email.ComposeAsync(curriculum);
+        }
+
+        private int LLenado()
+        {
+                int contador = 0;
+
+                if (!string.IsNullOrEmpty(redesxy))
+                {
+                    contador++;
+                }
+                if (!string.IsNullOrEmpty(educacionxy))
+                {
+                    contador++;
+                }
+                if (!string.IsNullOrEmpty(experienciaxy))
+                {
+                    contador++;
+                }
+                if (!string.IsNullOrEmpty(hobbiesxy))
+                {
+                    contador++;
+                }
+
+                return contador;
+        }
+
+
+        private bool Validar()
+        {
+
+                llenado = LLenado();
+
+                if (llenado == 4)
+                {
+                    return true;
+                }
+                else
+                {
+                    DisplayAlert("Error", "LLene todos los datos, por favor", "Ok");
+                    return false;
+                }
+        }
+
+        
+    }
+
+
+}
---
+++ Formulario CV/Vistas/Perfil.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Formulario_CV.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Perfil : ContentPage
+    {
+
+        String names;
+        String edads;
+        String correos;
+        String telefonos;
+        String ocupacions;
+        String direcions;
+        bool validacion;
+
+        public Perfil(String name, String edad,
+            String correo, String telefono, String ocupacion,
+            String direcion)
+        {
+            InitializeComponent();
+            names = name;
+            edads = edad;
+            correos = correo;
+            telefonos = telefono;
+            ocupacions = ocupacion;
+            direcions = direcion;
+
+        }
+
+        string Info_perfil;
+
+        private async void btncompletado_Clicked(object sender, EventArgs e)
+        {
+
+            Info_perfil = info_perfil.Text;
+
+            validacion = Validar();
+
+            if (validacion == true)
+            {
+                await Navigation.PushAsync(new Idiomas(names, edads,
+                correos, telefonos, ocupacions,
+                direcions, Info_perfil));
+            }
+        }
+
+        private bool Validar()
+        {
+            if (!string.IsNullOrEmpty(Info_perfil))
+            {
+                return true;
+            }
+            else
+            {
+                DisplayAlert("Error", "LLene todos los datos, por favor", "Ok");
+                return false;
+            }
+        }
+    }
+
+
+}
